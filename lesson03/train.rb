ef593883a977b958e959b2d72973cd33f39ae076
Master <vertupro061@gmail.com>
2021-04-18 09:20:04 +0300

    fix again

--- lesson03/train.rb
+++ lesson03/train.rb
@@ -2,26 +2,27 @@ class Train
   attr_accessor :wagons, :speed
   attr_reader :number, :type
 
-  def initialize(number, type, wagons) # Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
+  def initialize(number, wagons) # Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
     @number = number.to_s
-    @type = type
+    puts "Выбрите тип поезда: passenger или cargo"
+    @type = gets.chomp
     @wagons = wagons.to_i
     @speed = 0
   end
 
   def increase_speed # Может набирать скорость
-    self.speed += 5
+    @speed += 5
   end
 
   def stop_train # Может тормозить (сбрасывать скорость до нуля)
-    self.speed = 0
+    @speed = 0
   end
 
   # Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов). Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
 
   def attach_wagon
     if @speed == 0
-      self.wagons += 1
+      @wagons += 1
     else
       puts "Сначала остановите поезд"
     end
@@ -29,7 +30,7 @@ class Train
 
   def unhook_wagon
     if @speed == 0 && @wagons > 0
-      self.wagons -= 1
+      @wagons -= 1
     else
       puts "Сначала остановите поезд или проверьте наличие вагонов"
     end
