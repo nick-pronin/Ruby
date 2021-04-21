8f1eeb68d32644fbf2ce28e8d9fdb0d6c03d1907
Master <vertupro061@gmail.com>
2021-04-21 04:39:55 +0300

    added cargo and passenger trains

---
+++ lesson04/station.rb
@@ -0,0 +1,24 @@
+class Station
+  attr_reader :trains, :name
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def take_train(train)
+    @trains << train
+  end
+
+  def send_train(train)
+    @trains.delete(train)
+  end
+
+  def show_trains
+    @trains.each { |train| puts "На станции находятся нижеуказанные поезда: #{train}" }
+  end
+
+  def show_trains_type(type)
+    @trains.select{ |train| train.type == type }
+  end
+end
---
+++ lesson04/train.rb
@@ -0,0 +1,68 @@
+class Train
+  attr_reader :number, :type, :wagons, :speed, :route
+
+  def initialize(number, type)
+    @number = number.to_s
+    @type = type
+    @wagons = []
+    @speed = 0
+  end
+
+  def increase_speed
+    @speed += 5
+  end
+
+  def stop_train
+    @speed = 0
+  end
+
+  def attach_wagon
+    if @speed == 0
+      @wagons += 1
+    else
+      puts 'Сначала остановите поезд'
+    end
+  end
+
+  def unhook_wagon
+    if @speed == 0 && @wagons > 0
+      @wagons -= 1
+    else
+      puts 'Сначала остановите поезд или проверьте наличие вагонов'
+    end
+  end
+
+  def get_route(route)
+    @route = route
+    @current_station = 0
+    current_station.take_train(self)
+  end
+
+  def current_station
+    @route.stations[@current_station]
+  end
+
+  def next_station
+    @route.stations[@current_station + 1]
+  end
+
+  def previous_station
+    @route.stations[@current_station - 1] if @current_station > 0
+  end
+
+  def move_forvard
+    if next_station
+      current_station.send_train(self)
+      next_station.take_train(self)
+      @current_station += 1
+    end
+  end
+
+  def move_backward
+    if previous_station
+      current_station.send_train(self)
+      previous_station.take_train(self)
+      @current_station -= 1
+    end
+  end
+end
